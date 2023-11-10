71fe2bc4dcaa5e94e226fea698fd8434aa7295e4
Tibet Tornaci <oofdere@hey.com>
2023-11-09 23:52:28 -0800

    [part2] 2.4: Course information step9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,31 +1,12 @@
 import Course from "./components/Course";
+import courses from "./data";
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half-stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
-
   return (
     <div>
-      <Course course={course} />
+      {courses.map((c) => (
+        <Course key={c.id} course={c} />
+      ))}
     </div>
   );
 };
